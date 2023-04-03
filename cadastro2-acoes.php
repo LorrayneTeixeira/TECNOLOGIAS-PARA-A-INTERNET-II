2550d6da379e540f575313c06afa07aa7c86655d
Lorrayne Teixeira <lorrayne_s.t@hotmail.com>
2023-04-03 19:31:13 -0300

    commit

--- cadastro2-acoes.php
+++ cadastro2-acoes.php
@@ -1,33 +1,31 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['alunos'])){
-    $_SESSION['alunos']=array();
+if (!isset($_SESSION["alunosc"])) {
+    $_SESSION["alunosc"] = array();
 }
+$arra = array();
+$arra[] = $_POST["nome-aluno"];
+$arra[] = $_POST["ra-aluno"];
+$arra[] = $_POST["curso-aluno"];
 
-$arra=array();
-$arra=$_POST["nome-aluno"];
-$arra=$_POST["ra-aluno"];
-$arra=$_POST["curso-aluno"];
+$cont = 0;
+$parar = false;
 
-$cont=0;
-$parar=false;
+while ($cont < sizeof($_SESSION["alunosc"]) && $parar == false) {
+    $arrausu = $_SESSION["alunosc"];
+    $alunosc = $arrausu[$cont];
 
-while($cont<sizeof($_SESSION['alunos']) && $parar==false){
-    $arrausu= $_SESSION['alunos'];
-    $alunos=$arrausu[$cont];
-
-    if($alunos[1]==$arra[1]){
-        $parar=true;
+    if ($alunosc[1] == $arra[1]) {
+        $parar = true;
     }
     $cont++;
 }
 
-if($parar==false){
-    $_SESSION['alunos'][]=$arra;
+if ($parar == false) {
+    $_SESSION["alunosc"][] = $arra;
     header("Location: cadastro2.php?valid=2");
-
-}else{
-    header("Location: visualizar.php?id=".$cont);
+} else {
+    header("Location: cadastro2.php?id=" . $cont);
 }
 ?>
